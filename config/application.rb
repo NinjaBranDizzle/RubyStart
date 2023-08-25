3ee6ebc04934df7a4b92576e9c3c7d838acd66bd
Brandon Elfring <brandonelfring@gmail.com>
2023-08-25 15:07:49 -0600

    Install rspec for testing

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module RubyStart
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.active_job.queue_adapter = :sidekiq
+    #Rspec for testing
+    config.generators.test_framework :rspec
 
     # Configuration for the application, engines, and railties goes here.
     #
